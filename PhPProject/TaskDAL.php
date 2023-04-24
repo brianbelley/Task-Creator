d7d7cec969e16c119dc1b001f04f59cd718e712a
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2023-04-24 14:42:59 -0400

    modify the query on getTaskByTaskId() to get the task sorted

--- PhPProject/TaskDAL.php
+++ PhPProject/TaskDAL.php
@@ -22,9 +22,7 @@ class TaskDAL {
         mysqli_close($conn);
         return $tasks;
     }
-    
-    
-    
+
     public static function getTaskByTaskId($taskId) {
         $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         $query = "SELECT task.taskId, user.userId, user.firstName, user.lastName, task.description, task.date FROM task INNER JOIN user ON task.userId = user.userId WHERE task.taskId = $taskId";
